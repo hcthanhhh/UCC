cd8ccb2040fdc014f0a7735dede65613c58bcb4c
Chan-Thanh Hong <hcthanh@Chan-Thanhs-iMac.local>
2020-04-07 12:39:04 +0700

    add Mac request

--- Backend/src/controller/ApiController.js
+++ Backend/src/controller/ApiController.js
@@ -1,6 +1,8 @@
 const download = require('download-git-repo');
 const shell = require('node-powershell');
 const csv = require('csv-parser');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 const fs = require('fs');
 
 exports.CloneGit = (req, res) => {
@@ -13,7 +15,6 @@ exports.CloneGit = (req, res) => {
         console.log(e);
     });
     res.status(200).send('Success');
-    // console.log('success');
 }
 exports.UCCaUrl = async (req, res) => {
     request = req.body;
@@ -26,24 +27,44 @@ exports.UCCaUrl = async (req, res) => {
         noProfile: true
     });
 
-    // ps.addCommand('cd ../../');
     await ps.addCommand(`./UCC/UCC -unified -dir ../data/${username}/${name} -outdir ../data/${username}/${name}/result`);
     await ps.invoke().then(output => console.log(`res: ${output}`));
 
     var result = []
-    // res.attachment(`./data/${username}/${name}/result/TOTAL_outfile.csv`);
     fs.createReadStream(`../data/${username}/${name}/result/TOTAL_outfile.csv`)
         .pipe(csv())
         .on('data', row => {
             result.push(row);
         })
         .on('end', () => {
-            res.write(JSON.stringify(result));
+            res.json(result);
     });
 }
 
-exports.UCC2Url = () => {
+exports.UCC2Url = async (req, res) => {
     
+    request = req.body;
+    console.log(request);
+    username = req.body.username;
+    name = req.body.name;
+
+    try {
+        const {stdout, stderr} = await exec(`./UCC/UCC -unified -dir ../data/${username}/${name} -outdir ../data/${username}/${name}/result`);
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+    } catch (error) {
+        console.error(error);
+    };
+
+    var result = []
+    fs.createReadStream(`../data/${username}/${name}/result/TOTAL_outfile.csv`)
+        .pipe(csv())
+        .on('data', row => {
+            result.push(row);
+        })
+        .on('end', () => {
+            res.json(result);
+    });
 }
 exports.DeleteGit = () => {
 

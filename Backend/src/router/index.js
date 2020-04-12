be62fa77d6450cb23ad0f11a0484da9d16715a25
Chan-Thanh Hong <hcthanh@apcs.vn>
2020-04-12 10:10:41 +0700

    Delete Git

--- Backend/src/router/index.js
+++ Backend/src/router/index.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const {CloneGit, UCC2Url, UCCaUrl, UpdateGit, DeleteGit, UploadProject} = require('../controller/ApiController')
+const {
+    CloneGit, 
+    UCC2Url, 
+    UCCaUrl, 
+    UpdateGit, 
+    DeleteGit, 
+    UploadProject
+} = require('../controller/ApiController');
 
 const upload = multer({dest: '../data'});
 
-router.post('/CloneGit', CloneGit)
-router.post('/DeleteGit', DeleteGit)
-router.post('/UCCaUrl', UCCaUrl)
-router.post('/UCC2Url', UCC2Url)
-router.post('/UploadProject',upload.single('file'), UploadProject)
+router.post('/CloneGit', CloneGit);
+router.post('/DeleteGit', DeleteGit);
+router.post('/UCCaUrl', UCCaUrl);
+router.post('/UCC2Url', UCC2Url);
+router.post('/UploadProject',upload.single('file'), UploadProject);
 
 module.exports = router;

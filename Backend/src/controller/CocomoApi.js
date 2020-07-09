fd91403b299a091d6e7197e38f31b68a27598eab
Thanh Hong <hcthanh@apcs.vn>
2020-07-10 01:08:05 +0700

    intermediate Cocomo

--- Backend/src/controller/CocomoApi.js
+++ Backend/src/controller/CocomoApi.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const csv = require('csv-parser');
+const { time } = require('console');
 
 function getsize(username, name) {
     return new Promise((resolve, reject) => {
@@ -15,51 +16,94 @@ function getsize(username, name) {
     })
 }
 
+function isUndefined(value) {
+    // Obtain `undefined` value that's
+    // guaranteed to not have been re-assigned
+    var undefined = void (0);
+    return value === undefined;
+}
+
 exports.BasicCocomo = async (req, res) => {
     let request = req.body;
+    username = request.username;
+    name = request.name;
+    mode = request.mode;
+
+    console.log('Basic Cocomo: ', username, name);
+
     let size = 0;
-    let check = 0;
-
-    size = await getsize(request.username, request.name);
+    if (isUndefined(request.size)) size = await getsize(username, name);
+    else size = request.size;
     size = size / 1000;
-    console.log(size);
 
-    let model = ['Organic', 'Semi-Detached', 'Embedded'];
+    let model = {
+        '0': [2.4, 1.05, 2.5, 0.38],
+        '1': [3.0, 1.12, 2.5, 0.35],
+        '2': [3.6, 1.20, 2.5, 0.32]
+    };
 
-    let table = [2.4, 1.05, 2.5, 0.38, 3.0, 1.12, 2.5, 0.35, 3.6, 1.20, 2.5, 0.32];
+    effort = model[mode][0] * Math.pow(size, model[mode][1]);
+    EstimateTime = model[mode][2] * Math.pow(effort, model[mode][3]);
+    staff = Math.round(effort / EstimateTime) > 1 ? Math.round(effort / EstimateTime) : 1;
 
-    // Organic
-    if (size <= 50)
-        check = 0;
-
-    // Semi-detached
-    else if (size > 50 && size <= 300)
-        check = 1;
-
-    // Embedded 
-    else if (size > 300)
-        check = 2;
-
-    // Size < 2
-    else {
-        res.status(403).send({ message: 'Error' });
-        return;
-    }
-
-    let effort = table[0 + 4 * check] * Math.pow(size, table[1 + 4 * check]);
-    let time = table[2 + 4 * check] * Math.pow(effort, table[3 + 4 * check]);
-    let staff = Math.round(effort / time);
-    if (staff == 0) staff = 1;
-    res.status(200).send({
-        model: model[check],
-        effort: effort,
-        time: time,
-        staff: staff
+    res.status(200).json({
+        'effort': effort,
+        'time': EstimateTime,
+        'staff': staff
     });
 }
 
-exports.IntermediateCocomo = (req, res) => {
+exports.IntermediateCocomo = async (req, res) => {
+    let request = req.body;
+    username = request.username;
+    name = request.name;
+    drivers = request.drivers;
+    mode = request.mode;
 
+    console.log('Intermediate Cocomo: ', username, name);
+
+    attributes = {
+        '0': [0.75, 0.88, 1, 1.15, 1.4],
+        '1': [1, 1, 1, 1.08, 1.16],
+        '2': [0.7, 0.85, 1, 1.15, 1.3],
+        '3': [1, 1, 1, 1.11, 1.3],
+        '4': [1, 1, 1, 1.06, 1.21],
+        '5': [1, 0.87, 1, 1.15, 1.3],
+        '6': [1, 0.94, 1, 1.07, 1.15],
+        '7': [1.46, 1.19, 1, 0.86, 0.71],
+        '8': [1.29, 1.13, 1, 0.91, 0.82],
+        '9': [1.42, 1.17, 1, 0.86, 0.7],
+        '10': [1.21, 1.1, 1, 0.9, 1],
+        '11': [1.14, 1.07, 1, 0.95, 1],
+        '12': [1.24, 1.1, 1, 0.91, 0.82],
+        '13': [1.24, 1.1, 1, 0.91, 0.83],
+        '14': [1.23, 1.08, 1, 1.04, 1.1]
+    }
+
+    driver = 1;
+    i = 0;
+    drivers.forEach(element => driver *= attributes[i][element]);
+
+    let size = 0;
+    if (isUndefined(request.size)) size = await getsize(username, name);
+    else size = request.size;
+    size = size / 1000;
+
+    let model = {
+        '0': [2.4, 1.05, 2.5, 0.38],
+        '1': [3.0, 1.12, 2.5, 0.35],
+        '2': [3.6, 1.20, 2.5, 0.32]
+    };
+
+    effort = model[mode][0] * Math.pow(size, model[mode][1]) * driver;
+    EstimateTime = model[mode][2] * Math.pow(effort, model[mode][3]);
+    staff = Math.round(effort / EstimateTime) > 1 ? Math.round(effort / EstimateTime) : 1;
+
+    res.status(200).json({
+        'effort': effort,
+        'time': EstimateTime,
+        'staff': staff
+    });
 }
 
 exports.DetailedCocomo = (req, res) => { }
